ce87ae522fe3513b84695cf8b388d66e4278f975
fabien lacorre <fab.lacorre@gmail.com>
2018-09-04 11:53:50 +0200

    decorator pattern

---
+++ DecoratorPattern/src/main.cpp
@@ -0,0 +1,75 @@
+# include <iostream>
+
+// ABSTRACT CLASS FOR BEVERAGE //
+class IBeverage {
+public:
+	virtual int GetCoast() const = 0;
+protected:
+	int coast;
+};
+// -------------------------- //
+
+// COFFE CLASS -> WE PUT DECORATOR AROUND HER //
+class Coffe : public IBeverage{
+public:
+	Coffe(int coast);
+	~Coffe();
+	int GetCoast() const override;
+protected:
+	int coast;
+};
+
+Coffe::Coffe(int coast){
+	this->coast = coast;
+}
+
+Coffe::~Coffe(){
+
+}
+
+int Coffe::GetCoast() const {
+	return this->coast;
+}
+// ------------------------------------------ //
+
+// DECORATOR //
+class AddonDecorator : public IBeverage{
+public:
+	int GetCoast() const = 0;
+
+};
+
+class MilkDecorator : public AddonDecorator {
+public:
+	MilkDecorator(IBeverage &b, int coast);
+	~MilkDecorator() {};
+	int GetCoast() const override;
+private:
+	IBeverage *b;
+};
+
+MilkDecorator::MilkDecorator(IBeverage &b, int coast){
+	this->b = &b;
+	this->coast = coast;
+}
+
+int MilkDecorator::GetCoast() const {
+	return this->b->GetCoast() + this->coast;
+}
+// --------- //
+
+int main(){
+	// coffe coast 10 //
+	Coffe coffe(10);
+	// We add milk decorator on coffe //
+	MilkDecorator milkyCoffe(coffe, 5);
+	// We add again milk decorator on milky coffe //
+	MilkDecorator doublemilkyCoffe(milkyCoffe, 5);
+	/*
+	 When we print coast of double milky coffe we have 20 
+	 This is a price of coffe + milk + milk
+	 Coffre is wrap twice by milk 
+	*/
+	std::cout << doublemilkyCoffe.GetCoast() << std::endl;
+	return (0);
+}
